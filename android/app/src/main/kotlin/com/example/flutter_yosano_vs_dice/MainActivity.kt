8e8265f8a3694116562eb316ea00a3fc181eb5cd
dicenull <dicenull.dev@gmail.com>
2022-06-23 22:45:59 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_yosano_vs_dice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_yosano_vs_dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.blue[100],
+    );
+  }
+}
